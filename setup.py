a115a2903b342730f4700ee8e1c0abaf1a15ff29
Shane Graber <sgraber@gmail.com>
2010-09-23 21:25:41 -0400

    added install.txt, license.txt to long_description

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(name='aaolm.portlet.facebook',
       version=version,
       description="Facebook Fan Page Portlet for Advanced Aquarist",
       long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "INSTALL.txt")).read()  + "\n" +
+                       open(os.path.join("docs", "LICENSE.txt")).read()  + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
